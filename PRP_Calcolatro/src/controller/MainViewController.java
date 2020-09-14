842e30d936372868375635b032c0337f5b448e62
Adam Gierek <gierek.adam93@gmail.com>
2020-09-14 20:46:20 +0200

    Simple Calculator using Javafx

---
+++ PRP_Calcolatro/src/controller/MainViewController.java
@@ -0,0 +1,66 @@
+package controller;
+
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainViewController {
+    @FXML
+    private Button ac;
+
+    @FXML
+    private Button plusMinus;
+
+    @FXML
+    private Button percent;
+
+    @FXML
+    private Button b7;
+
+    @FXML
+    private Button devide;
+
+    @FXML
+    private Button b8;
+
+    @FXML
+    private Button multiply;
+
+    @FXML
+    private Button b4;
+
+    @FXML
+    private Button b5;
+
+    @FXML
+    private Button b6;
+
+    @FXML
+    private Button minus;
+
+    @FXML
+    private Button b1;
+
+    @FXML
+    private Button b2;
+
+    @FXML
+    private Button b3;
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button b0;
+
+    @FXML
+    private Button dot;
+
+    @FXML
+    private Button equal;
+
+    @FXML
+    private Button b9;
+
+}
